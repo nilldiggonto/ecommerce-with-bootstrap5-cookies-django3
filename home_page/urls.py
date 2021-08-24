919438b74adbc4e92407d556caae9658cf42be27
nilldiggonto <nilldiggonto@gmail.com>
2021-08-24 13:15:09 +0600

    working with category

---
+++ home_page/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',homeView,name='home-page'),
+    path('category/',category,name='category'),
+]
--- home_page/views.py
+++ home_page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homeView(request):
+    template_name = 'home/home.html'
+    return render(request,template_name)
+
+def category(request):
+    template_name = 'home/category.html'
+    return render(request,template_name)
